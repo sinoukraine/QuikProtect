628a6a7aab913d71de21cd1a1559e390886e8391
Yuriy <colortrue1992@gmail.com>
2019-03-27 12:23:24 +0200

    ios

--- www/resources/app/helper.js
+++ www/resources/app/helper.js
@@ -23,11 +23,6 @@ Helper ={
             popupAnchor:  [0, -60] // point from which the popup should open relative to the iconAnchor
         })
     ],
-    StatusNewEnum:{
-        "geolock" : 1,
-        "immob": 2,
-        "doorlock": 4,
-    },
     getDirectionCardinal: function(direction){
             var ret = "";
             direction = parseFloat(direction);
@@ -104,7 +99,6 @@ Helper ={
         }
         return ret;
     },
-
     getAddressByGeocoder: function(latlng,replyFunc){
         var url = "https://nominatim.sinopacific.com.ua/reverse.php?format=json&lat={0}&lon={1}&zoom=18&addressdetails=1".format(latlng.lat, latlng.lng);
         var coords = latlng.lat + ', '+ latlng.lng;
